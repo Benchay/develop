214336788285e3219be6b6f9ad092e2650def817
chenchangchun <767098186@qq.com>
2018-03-06 12:01:43 +0800

    3.6

--- src/pages/spreadcenter/commonvue/periodspreadsever.vue
+++ src/pages/spreadcenter/commonvue/periodspreadsever.vue
@@ -4,39 +4,81 @@
         <el-input v-model="input" placeholder="请输入内容" style="width: 400px;"></el-input>
          <el-button style="background: #205081; color: #ffffff; margin: 0 15px;">选择执行方案</el-button>
          <span>当前已选方案 : 模版一</span>
+          <div style="float: right; margin-bottom: 20px;">
+              <el-checkbox v-model="showdeal">显示Deal活动购买件数</el-checkbox>
+          </div>
       </div>
-      <div id="bottommain">
-      <el-tabs v-model="editableTabsValue" type="card" editable @edit="handleTabsEdit">
-        <el-tab-pane
-            :key="item.name"
-            v-for="(item, index) in editableTabs"
-            :label="item.title"
-            :name="item.name"
-        >
-            <div class="tablewrap">
-                <el-table
-                :data="tableData"
-                style="width: 100%">
-                <el-table-column
-                    prop="date"
-                    label="行为名称">
-                </el-table-column>
-                <el-table-column
-                    label="任务数量">
-                    <template slot-scope="scope">
-                        <el-input-number v-model="num1" @change="handleChange" :min="1" label="描述文字"></el-input-number>
-                    </template>
-                </el-table-column>
-                <el-table-column
-                    label="购买件数/单">
-                    <template slot-scope="scope">
-                        <el-input-number v-model="num1" @change="handleChange" :min="1" label="描述文字"></el-input-number>
-                    </template>
-                </el-table-column>
-                </el-table> 
+      <div class="bottommain">
+          <div class="tabHeader">
+                <div>
+                    <p>周期天数</p>
+                    <p style="color: #ee811e;">新增</p>
+                </div>
+                <div>
+                    <p>普通点击</p>
+                    <p style="color: #ee811e;">($0.2/个)</p>
+                </div>
+                <div>
+                    <p>普通加购</p>
+                    <p style="color: #ee811e;">($0.5/个)</p>
+                </div>
+                <div>
+                    <p>普通收藏</p>
+                    <p style="color: #ee811e;">($0.5/个)</p>
+                </div>
+                <div class="sellNum">
+                    <span class="left">刷销量<span style="color: #ee811e;">($20.00/个)</span></span>
+                    <span class="right">每单购买数</span>
+                </div>
             </div>
-        </el-tab-pane>
-        </el-tabs>
+            <div class="tabBody">
+                <div class="tabBodyItem">
+                    <div class="entrySearch">
+                        <span class="insbefore" style="background: #efabaf;"><i><img src="./entrance_1.png" alt=""></i>关键词</span>
+                        <span class="insafter">X</span>
+                    </div>
+                     <div class="sellinputNum">
+                            <el-input-number v-model="num1"  @change="handleChange" :min="1" label="描述文字"></el-input-number>
+                        </div>
+                         <div class="sellinputNum">
+                            <el-input-number v-model="num1"  @change="handleChange" :min="1" label="描述文字"></el-input-number>
+                        </div>
+                         <div class="sellinputNum">
+                            <el-input-number v-model="num1"  @change="handleChange" :min="1" label="描述文字"></el-input-number>
+                        </div>
+                    <div class="sonumWrap">
+                        <div class="sellinputNum">
+                            <el-input-number v-model="num1"  @change="handleChange" :min="1" label="描述文字"></el-input-number>
+                        </div>
+                        <div class="orderinputNum" v-if="showdeal">
+                            <el-input-number v-model="num2" @change="handleChange" :min="1" label="描述文字"></el-input-number>
+                        </div>
+                     </div>
+                </div>
+                <div class="tabBodyItem">
+                    <div class="entrySearch">
+                        <span class="insbefore" style="background: #4499db;"><i><img src="./entrance_2.png" alt=""></i>Today's deal</span>
+                        <span class="insafter">X</span>
+                    </div>
+                     <div class="sellinputNum">
+                            <el-input-number v-model="num1"  @change="handleChange" :min="1" label="描述文字"></el-input-number>
+                        </div>
+                         <div class="sellinputNum">
+                            <el-input-number v-model="num1"  @change="handleChange" :min="1" label="描述文字"></el-input-number>
+                        </div>
+                         <div class="sellinputNum">
+                            <el-input-number v-model="num1"  @change="handleChange" :min="1" label="描述文字"></el-input-number>
+                        </div>
+                    <div class="sonumWrap">
+                        <div class="sellinputNum">
+                            <el-input-number v-model="num1"  @change="handleChange" :min="1" label="描述文字"></el-input-number>
+                        </div>
+                        <div class="orderinputNum" v-if="showdeal">
+                            <el-input-number v-model="num2" @change="handleChange" :min="1" label="描述文字"></el-input-number>
+                        </div>
+                     </div>
+                </div>
+            </div>
         <div class="btnwrap">
             <el-button style="background: #205081; color: #ffffff;">保存方案</el-button>
         </div>
@@ -47,8 +89,8 @@
   export default {
     data() {
       return {
+         showdeal: true,
         input: '',
-        editableTabsValue: '2',
         editableTabs: [{
           title: '第1天',
           name: '1',
@@ -70,72 +112,120 @@
       }
     },
     methods: {
-      handleTabsEdit(targetName, action) {
-        if (action === 'add') {
-          let newTabName = ++this.tabIndex + '';
-          this.editableTabs.push({
-            title: '第'+this.tabIndex+'天',
-            name: newTabName,
-            content: 'New Tab content'
-          });
-          this.editableTabsValue = newTabName;
-        }
-        if (action === 'remove') {
-          let tabs = this.editableTabs;
-          let activeName = this.editableTabsValue;
-          if (activeName === targetName) {
-            tabs.forEach((tab, index) => {
-              if (tab.name === targetName) {
-                let nextTab = tabs[index + 1] || tabs[index - 1];
-                if (nextTab) {
-                  activeName = nextTab.name;
-                }
-              }
-            });
-          }
-          
-          this.editableTabsValue = activeName;
-          this.editableTabs = tabs.filter(tab => tab.name !== targetName);
-        }
-      },
-    handleChange(value) {
-        console.log(value);
-      }
+    
     }
   }
 </script>
 <style lang="scss">
     .periodspreadsever{
         .topword{
-          padding-left: 60px;
-          margin-bottom: 20px;
+          overflow: hidden;
+          padding: 0 20px 15px 80px;
+          margin: 0 -20px;
           position: relative;
+          border-bottom: 1px solid #cccccc;
           &:before{
               content: '关键词:';
               width: 60px;
               height: 40px;
               background-color: #fff;
               position: absolute;
-              left: 0;
+              left: 20px;
               top: 8px;
           }
         }
-        #bottommain{
-            .el-tabs--card > .el-tabs__header .el-tabs__item{
-                margin-right: 1px;
+        .bottommain{
+          padding-top: 20px;
+          .tabHeader{
+            padding: 0 30px;
+            display: flex;
+            margin-bottom: 10px;
+            div{
+                flex: 1;
+                height: 50px;
+                text-align: center;
+                background-color: #f5f5f5;
+                margin-right: 20px;  
+                border-radius: 20px 20px 0 0;
                 border: 1px solid #cccccc;
-            }
-            .el-tabs--card > .el-tabs__header .el-tabs__item.is-active{
-                transform: translateY(0px);
-                border: 1px solid #205081;
-            }
-            .tablewrap{
-                border: 1px solid #e6e6e6;
-                margin-top: 20px;
-                .el-table thead tr th{
-                    background-color: #edf5fc;
+                overflow: hidden;
+                &:last-child{
+                margin-right: 0px;  
+                }
+                &.sellNum{
+                    flex: 2;
+                    display: flex;
+                    justify-content: space-around;
+                    padding-top: 5px;
+                    box-sizing: border-box;
+                }
+                span{
+                    height: 38px;  
+                    line-height: 38px;
+                    margin-left: 6px;
                 }
             }
+        }
+        .tabBody{
+            .tabBodyItem{
+                 display: flex;
+                 padding: 0 30px;
+                 margin-bottom: 20px;
+                &>div{
+                  flex: 1; 
+                  height: 40px;
+                  margin-right: 20px;   
+                    &:last-child{
+                margin-right: 0px;  
+                }
+                }
+                .entrySearch{
+                    display: flex;
+                    .insbefore{
+                        flex: 1;
+                        text-align: right;
+                        line-height: 40px;
+                        border-radius: 20px 0 0 20px;
+                        background-color: #75d282;
+                        padding-right: 15px;
+                        color: #ffffff;
+                        i{
+                            display: inline-block;
+                            vertical-align: top;
+                            margin-top: 4px;
+                            margin-right: 4px;
+                        }
+                    }
+                    .insafter{
+                        width: 40px;
+                        text-align: center;
+                        line-height: 40px;
+                        font-size: 20px;
+                        background-color: #fe6c6f;
+                        color: #ffffff;
+                    }
+                    .el-input__inner{
+                        flex: 1;
+                        border-radius: 0;
+                    }
+                }
+                .sonumWrap{
+                    flex: 2;
+                    display: flex;
+                    .sellinputNum{
+                        .el-input-number{
+                            width: 100%;
+                        }
+                    }
+                    .orderinputNum{
+                        margin-left: 15px;
+                        .el-input-number{
+                            width: 100%;
+                        }
+                    }
+                 }
+            }
+        }
             .btnwrap{
                 text-align: center;
                 margin-top: 20px;
